0f490db383bbfc9ec188a0bebbeb17b16a8d9b29
glagglag <vitalya.pile@gmail.com>
2022-05-03 02:00:26 +0900

    init

---
+++ src/main/java/ru/zabgu/internetshop/InternetshopApplication.java
@@ -0,0 +1,13 @@
+package ru.zabgu.internetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zabgu/internetshop/InternetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zabgu.internetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
